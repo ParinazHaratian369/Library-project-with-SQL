7d4310d6d32c0adeda55300415f87cdcf6311d84
Parinaz <haratian.p@gmail.com>
2025-04-21 03:11:01 +0330

    importing Database class from database.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import customtkinter as ctk
+from database import Database
 class BookstoreApp:
     def __init__(self, root):
         self.root = root
-        self.root.title = "Bookstore Management"
-        self.root.geometry = "300x400"
+        self.root.title("Bookstore Management")
+        self.root.geometry("400x500")
+
 
 if __name__ == "__main__":
     root = ctk.CTk()
