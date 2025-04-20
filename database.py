35ae7d3f1a8b1f91eb0881ec61202d1005c02f14
Parinaz <haratian.p@gmail.com>
2025-04-21 03:04:43 +0330

    initializing database connectin to bookstore

--- database.py
+++ database.py
@@ -2,4 +2,9 @@ import mysql.connector
 
 class Database:
     def __init__(self):
-        pass
+        self.connection = mysql.connector.connect(
+            host = "localhost",
+            user = "root",
+            password = "12345678",
+            database = "bookstore"
+        )
