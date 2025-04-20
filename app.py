8caface7696aabeda24f32a9b1bf80408fdf2a7c
Parinaz <haratian.p@gmail.com>
2025-04-21 02:41:41 +0330

    changing the title and geometry of Bookstore window and run it

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import customtkinter as ctk
 class BookstoreApp:
     def __init__(self, root):
         self.root = root
+        self.root.title = "Bookstore Management"
+        self.root.geometry = "300x400"
 
 if __name__ == "__main__":
     root = ctk.CTk()
