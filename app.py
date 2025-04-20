3cd0870363aa2ea248592bffb880bad01e3ed7ac
Parinaz <haratian.p@gmail.com>
2025-04-21 02:31:36 +0330

    creating an object from BookstoreApp and sending root window as a parameter to it

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import customtkinter as ctk
 class BookstoreApp:
-    def __init__(self):
+    def __init__(self, root):
         pass
-    
+
 if __name__ == "__main__":
     root = ctk.CTk()
+    app = BookstoreApp(root)
